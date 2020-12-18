e917ee772511fd7842be0cd6e70ca99f68742d7d
Emile Normand <emile.normand.1@ens.etsmtl.ca>
2020-12-17 19:56:22 -0500

    replacing python client with raw HTTP request

--- interop-client.py
+++
@@ -1,9 +0,0 @@
-from interop.client.auvsi_suas.client import client
-#from interop.client.auvsi_suas.proto import interop_api_pb2
-
-client = client.Client(url='http://127.0.0.1:8000',
-                       username='testuser',
-                       password='testpass')
-
-teams = client.get_teams()
-print(teams)
